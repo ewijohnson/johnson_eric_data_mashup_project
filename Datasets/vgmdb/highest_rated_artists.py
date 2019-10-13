40f3ee227cd6ae2689b742a8a7734e872af84f3c
ewijohnson <ericwj2@illinois.edu>
2019-10-12 19:25:56 -0500

    ready to start scraping for vgmdb Most Popular Artists

--- Datasets/vgmdb/highest_rated_artists.py
+++ Datasets/vgmdb/highest_rated_artists.py
@@ -3,16 +3,16 @@ import requests
 import time
 import random
 
-with open('least_popular_albums.txt', 'w', encoding='utf-8') as outfile:
+with open('highest_rated_artists.txt', 'w', encoding='utf-8') as outfile:
 
-    print("Rank, Album Name, Rating (#votes), Popularity", file=outfile)
+    print("Rank, Artist Name, Rating (#votes)", file=outfile)
 
-    base_url = "https://vgmdb.net/db/statistics.php?do=bottom_rated"
+    base_url = "https://vgmdb.net/db/statistics.php?do=top_rated_artists"
 
     page_num_ending = ''
     page_num = None
 
-    while page_num != 99:  # 98 pages of data as of 2019-10-12
+    while True:
 
         if page_num is None:
             print('Gathering data for page 1...')
@@ -25,11 +25,13 @@ with open('least_popular_albums.txt', 'w', encoding='utf-8') as outfile:
         tree = html.fromstring(page.content)
 
         # tree = html.parse("C:/Users/oboec/MSLIS/IS590OMO/johnson_eric_data_mashup_project/Datasets/vgmdb/Webpage
-        # Local Copy/Least Popular Albums (10 vote minimum) - VGMdb.html")
+        # Local Copy/Highest Rated Artists (20 vote minimum) - VGMdb.html")
 
-        rankings = tree.xpath("//tr/td/text() | //tr/td/a//span[@lang='en']/text()")
+        rankings = tree.xpath("//tr/td/text() | //tr/td/a/text()")
 
         num = 0
+        page_one_break_num = -1
+
         for rank in rankings:
             rank = rank.strip('\n')
             rank = rank.strip('\t')
@@ -37,11 +39,11 @@ with open('least_popular_albums.txt', 'w', encoding='utf-8') as outfile:
             if rank[:5] == 'Page ':
                 break
             if rank != '':
-                if num in [0, 1, 2]:
+                if num in [0, 1]:
                     print(rank, end=', ', file=outfile)
                     print(rank)
                     num += 1
-                elif num == 3:
+                elif num == 2:
                     print(rank, file=outfile)
                     print(rank)
                     num = 0
@@ -52,5 +54,10 @@ with open('least_popular_albums.txt', 'w', encoding='utf-8') as outfile:
         else:
             page_num += 1
 
+        if page_num == 523:  # 522 pages of data, as of 2019-10-12
+            break
+
         s = random.uniform(8, 15)
+        print('Done with page ' + str(page_num - 1) + ', waiting ' +
+              str(s) + ' seconds for page ' + str(page_num) + '.')
         time.sleep(s)
