235af4b4a6a2d6c081b3cb4524292f1a28318380
ewijohnson <ericwj2@illinois.edu>
2019-11-17 14:37:04 -0600

    created all_composer_data text file, the combination of indiv files

--- Datasets/Wikipedia/List of Video Game Musicians/combining_indv_to_one.py
+++ Datasets/Wikipedia/List of Video Game Musicians/combining_indv_to_one.py
@@ -6,6 +6,22 @@ composer_files = []
 for files in os.listdir(path):
     composer_files.append(files)
 
-for file in composer_files:
-    with open((path + '/' + file), encoding='UTF-8') as f:
-        print(f.read())
+with open('all_composer_data.txt', 'w', encoding='UTF-8') as outfile:
+    print('Soundtrack, Composer, Composer Birthday', file=outfile)
+    for file in composer_files:
+        with open((path + '/' + file), encoding='UTF-8') as f:
+            f = f.readlines()
+            n = 0
+            for line in f:
+                if n < 4:
+                    if n == 0:
+                        composer = line.strip('\n')
+                        n += 1
+                    elif n == 2:
+                        birthday = line[(line.index(':'))+1:].lstrip()
+                        n += 1
+                    else:
+                        n += 1
+                else:
+                    line = line.strip('\n')
+                    print(line + ', ' + composer + ', ' + birthday, end='', file=outfile)
